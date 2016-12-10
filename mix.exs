2b8d48b7cf1f1df0d024f84b22f12eebadd40f1a
Milan Burmaja <burmajam@gmail.com>
2016-12-11 00:38:11 +0100

    Added info needed to publish project to hex

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule AlarmClock.Mixfile do
     [app: :alarm_clock,
      version: "0.0.1",
      elixir: "~> 1.3",
+     package: package,
+     description: description,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -18,7 +20,24 @@ defmodule AlarmClock.Mixfile do
 
   defp deps do
     [
-      {:calendar, "~> 0.16.1"}
+      {:calendar, "~> 0.16.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
+  end
+
+  defp package do
+    %{
+       maintainers: ["Milan Burmaja"],
+       links: %{ "GitHub" => "https://github.com/burmajam/alarm_clock"},
+       licenses: ["MIT"],
+       files: ~w(lib mix.exs README*) }
+  end
+
+  defp description do
+    """
+    :timer.send_after/3 persistable. Allows scheduling message delivery to specified process in miliseconds
+    or at specified Calendar.DateTime. AlarmClock will make sure that messages are delivered using retry mechanism
+    and after it's own crash, since all messages can be persisted.
+    """
   end
 end
